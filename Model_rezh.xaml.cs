15d4af61192928e2a707e817f8321c0d36b5a227
Светлана Блаженова <tirend1403fluorit@gmail.com>
2021-11-02 00:21:57 +0300

    Задание режимных параметров

--- Model_rezh.xaml.cs
+++ Model_rezh.xaml.cs
@@ -20,9 +20,23 @@ namespace БД_НТИ
     /// </summary>
     public partial class Model_rezh : Page
     {
+        int chan = 0; //номер канала (начиная с 0 (т.е. №-1))
         public Model_rezh()
         {
             InitializeComponent();
+            for (int i = 1; i < Data.channels.Count; i++)
+            {
+                RadioButton radio = new RadioButton { Content = $"Канал {i + 1}", BorderBrush = new SolidColorBrush(Color.FromRgb(0, 14, 153)) };
+                radio.Checked += new RoutedEventHandler(RadioButton_Checked);
+                stackpanel_chan.Children.Add(radio);
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var radio = sender as RadioButton;
+            string[] names = radio.Content.ToString().Split(' ');
+            chan = Convert.ToInt32(names[1]) - 1; //номер канала (начиная с 0 (т.е. №-1))
         }
     }
 }
